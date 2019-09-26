3b12447b54e445514785b85a99f3f566ce68453c
Jonathan <jon.michael.edwards13@gmail.com>
2019-09-26 15:34:22 +1200

    question 1

--- Question_1.m
+++ Question_1.m
@@ -56,15 +56,17 @@ time2008 = (y1t + y2t + y3t) / 3;
 
 
 % ------------------ Plotting ------------------ %
-figure(1)
+figure(1);
 plot(x, y1, x, y2, x, y3);
 xticks(0:10:80);
 xticklabels(1928:10:2010);
+xlim([-4 84]);
+ylim([10.4 12.3]);
 hold on;
 scatter(x, y);
 scatter(2008-1928, time2008);
 hold off;
-xlabel('Years since 1928');
+xlabel('Years');
 ylabel('Time (s)');
 title('Least squares approximation for degrees 1, 2 and 3');
 legend('Deg 1', 'Deg 2', 'Deg 3', 'Test data', 'Predicted data');

2363c5dabffb6ff3d434897d356d8a5ab13bbc87
William Ieremia Herewini <wih15@cs18250kq.canterbury.ac.nz>
2019-09-28 11:48:33 +1200

    im still cool

--- Question_3.m
+++ Question_3.m
@@ -3,13 +3,26 @@ clear, clc
 % Loading in test data
 load('A1.mat')
 colormap(gray(256));
-image(A);
 
 [U, S, V] = svd(A);
 A_sz = size(A);
-A_sv = 0*A_sz;
+A_sv = zeros(1, A_sz(1));
 
 for i = 1:A_sz(1)
     A_sv(i) = S(i, i);
 end
 A_sv_range = [max(A_sv), min(A_sv)];
+
+image_sv = [2 6 10 15 20 30 50 100];
+A_sv_com = zeros(1, length(image_sv));
+
+for j = 1:length(image_sv)
+    new_mat = zeros(A_sz(1), A_sz(2));
+    for k = 1:image_sv(j)
+        new_mat(k, k) = A_sv(k);
+    end
+    figure(j)
+    image(U*new_mat*V');
+end
+
+% image(A_sv_com(8));
